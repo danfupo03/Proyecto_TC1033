06701923608db01bcbe9c31c63079e74b727bf8e
Daniel Fuentes <94033808+danfupo03@users.noreply.github.com>
2021-11-25 21:50:13 -0600

    Update Anfibios.h

--- Anfibios.h
+++ Anfibios.h
@@ -1,74 +1,28 @@
 #include<string>
+#include "Animales.h"
 
 using namespace std;
 
-class Anfibios
+class Anfibio: public Animal
 {
-    private:
-        string especie;
-        int poblacion;
-        int mortalidad = 0;
-        int i = 0;
-        int años;
 
     public:
         //Constructores
-        Anfibios(): especie(""), poblacion(0), años(0){};
-        Anfibios(string esp, int pob, int y): especie(esp), poblacion(pob), años(y){};
+        Anfibio(string esp, int pob): Animal(esp, pob, 0){
+        }
 
-        string get_especie();
-        int get_poblacion();
-        int get_años();
 
-        void set_especie(string );
-        void set_poblacion(int );
-        void set_años(int );
-    
-        void aumento_poblacion(int );
-
-};
-
-//Getters
-string Anfibios::get_especie()
-{
-  return especie;
-}
-
-int Anfibios::get_poblacion()
-{
-  return poblacion;
-}
-
-int Anfibios::get_años()
-{
-  return años;
-}
-
-//Setters
-void Anfibios::set_especie(std::string esp)
-{
-  especie = esp;
-}
-
-void Anfibios::set_poblacion(int pob)
-{
-  poblacion = pob;
-}
-
-void Anfibios::set_años(int y)
-{
-  años = y;
-}
-
-void Anfibios::aumento_poblacion(int tiempo) //Función que calcula aproximadamente el crecimiento de la población de la especie.
-{
-    while(i < tiempo)
+    void aumentaPoblacionAnfibios(int tiempo) //Función que calcula aproximadamente el crecimiento de la población de la especie.
     {
-        mortalidad = poblacion - (poblacion * 0.5);
-        poblacion = mortalidad + (poblacion * 2);
-        i = i + 1;
+        while(i < tiempo)
+        {
+            mortalidad = poblacion - (poblacion * 0.5);
+            poblacion = mortalidad + (poblacion * 2);
+            i = i + 1;
+        }
+        años = años + tiempo;
     }
-    años = años + tiempo;
-}
+    
+};
 
 
