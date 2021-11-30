e27714569fcf75354a281cbabed21a3d6432a248
Daniel Fuentes <94033808+danfupo03@users.noreply.github.com>
2021-11-30 10:50:35 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,61 +1,98 @@
-#include <iostream>
-#include <string>
-#include "Mamiferos.h"
-#include "Reptiles.h"
-#include "Anfibios.h"
+/*
+ * Proyecto Reserva
+ * Daniel Emilio Fuentes Portaluppi
+ * A01708302
+ * 03/12/2021
+ */
 
+/**
+ * Descripcion este es un proyecto para la clase de Pensamiento
+ * Computacional Orientado a Objetos. Es un programa que captura
+ * diferentes tipos de animales con sus respectivas poblaciones, y nos
+ * permite verlos después. 
+*/
 
-int main()
-{
-    //Objeto Mamíferos
-    cout<<"----------Mamíferos----------"<<"\n"<<endl;
-    
-    Mamifero animal_1("Odocoileus virginianus (Venado de cola blanca)",50);
-    cout<<"La especie "<<animal_1.getEspecie()<<","<<endl;
-    cout<<"tiene una población de "<<animal_1.getPoblacion()<<" individuos,"<<endl;
-    cout<<"a los "<<animal_1.getAños()<<" años."<<"\n"<<endl;
-    
-    animal_1.aumentaPoblacionMamiferos(3);
-    
-    cout<<animal_1.getAños()<<" años despues..."<<"\n"<<endl;
-    
-    cout<<"La especie "<<animal_1.getEspecie()<<","<<endl;
-    cout<<"tiene aproximadamente "<<animal_1.getPoblacion()<<" individuos,"<<endl;
-    cout<<"despues de "<<animal_1.getAños()<<" años."<<"\n"<<endl;
-    
-    //Objeto Reptiles
-    cout<<"----------Reptiles----------"<<"\n"<<endl;
-    
-    Reptil animal_2("Crotaus asquilus (Cascabel oscura)", 50, 0);
-    cout<<"La especie "<<animal_2.getEspecie()<<","<<endl;
-    cout<<"tiene una población de "<<animal_2.getPoblacion()<<" individuos,"<<endl;
-    cout<<"a los "<<animal_2.getAños()<<" años."<<endl;
-    cout<<"Ha mudado de piel "<<animal_2.getPiel()<<" veces."<<"\n"<<endl;
-    
-    animal_2.aumentaPoblacionReptiles(3);
-    animal_2.mudaPiel(3);
-    
-    cout<<animal_2.getAños()<<" años despues..."<<"\n"<<endl;
-    
-    cout<<"La especie "<<animal_2.getEspecie()<<","<<endl;
-    cout<<"tiene aproximadamente "<<animal_2.getPoblacion()<<" individuos,"<<endl;
-    cout<<"despues de "<<animal_2.getAños()<<" años."<<endl;
-    cout<<"Uno de sus individuos ha mudado de piel aproximadamente "<<animal_2.getPiel()<<" veces."<<"\n"<<endl;
-    
-    //Objeto Anfibios
-    cout<<"----------Anfibios----------"<<"\n"<<endl;
-    
-    Anfibio animal_3("Rana linnaeus (Rana de estanque)",50);
-    cout<<"La especie "<<animal_3.getEspecie()<<","<<endl;
-    cout<<"tiene una población de "<<animal_3.getPoblacion()<<" individuos,"<<endl;
-    cout<<"a los "<<animal_3.getAños()<<" años."<<"\n"<<endl;
-    
-    animal_3.aumentaPoblacionAnfibios(3);
-    
-    cout<<animal_1.getAños()<<" años despues..."<<"\n"<<endl;
-    
-    cout<<"La especie "<<animal_3.getEspecie()<<","<<endl;
-    cout<<"tiene aproximadamente "<<animal_3.getPoblacion()<<" individuos,"<<endl;
-    cout<<"despues de "<<animal_3.getAños()<<" años."<<endl;
-    
+//Bibliotecas
+#include <iostream>   //para imprimir.
+#include "Animales.h" //donde estan los objetos de mi proyecto.
+#include "Reserva.h"
+
+using namespace std;
+
+//Procedimiento menu
+void menu(){
+
+    //Imprime las opciones que va a tener el sistema
+    cout << "Menu:\n";
+    cout << "1. Mostrar mamiferos. \n";
+    cout << "2. Mostrar reptiles. \n";
+    cout << "3. Mostrar anfibios. \n";
+    cout << "4. Agregar mamifero \n";
+    cout << "5. Agregar reptil \n";
+    cout << "6. Agregar anfibio \n";
+    cout << "7. Salir \n";
+}
+
+int main(int argc, char* argv[]){
+
+      Reserva reserva;
+      reserva.creaMamiferos();
+      reserva.creaReptiles();
+      reserva.creaAnfibios();
+      string temp_especie;
+      int temp_poblacion;
+      int temp_piel;
+      int opcion = 0;
+
+    //Ciclo para que el sistema siga corriendo mientras no elija la opcion salir.
+    while(opcion < 7 && opcion > -1){
+
+          //Impresion de menu
+          menu();
+          //Leer indice que selecciona la opcion del menu
+          cin >> opcion;
+
+          //Switch donde dependiendo la eleccion efectua un diferente procedimiento
+          switch(opcion){
+
+              //Caso 1 que imprime los empleados de tipo asalariado
+              case 1:
+                  reserva.muestraMamiferos();
+                break;
+
+              //Caso 2 que imprime los empleados de tipo Por Hora
+              case 2:
+                  reserva.muestraReptiles();
+                break;
+
+              //Caso 3 que imprime los empleados de tipo Practicantes
+              case 3:
+                  reserva.muestraAnfibios();
+                break;
+
+              case 4:
+                  cout<< "Dime la especie " ;
+                  cin >> temp_especie;
+                  cout<< "Dime la poblacion " ;
+                  cin >> temp_poblacion;
+                  reserva.agregaMamifero(temp_especie, temp_poblacion);
+                break;
+
+              case 5:
+                  cout<< "Dime la especie " ;
+                  cin >> temp_especie;;
+                  cout<< "Dime la poblacion " ;
+                  cin >> temp_poblacion;
+                  reserva.agregaReptil(temp_especie, temp_poblacion, temp_piel);
+                break;
+
+              case 6:
+                  cout<< "Dime la especie " ;
+                  cin >> temp_especie;
+                  cout<< "Dime la poblacion " ;
+                  cin >> temp_poblacion;
+                  reserva.agregaAnfibio(temp_especie, temp_poblacion);
+                break;
+          }
+    }
 }
