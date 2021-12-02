957974229545a7bc84a567475d56a810c128464c
Daniel Fuentes <94033808+danfupo03@users.noreply.github.com>
2021-12-02 17:17:46 -0600

    Update Reptiles.h

--- Reptiles.h
+++ Reptiles.h
@@ -1,4 +1,9 @@
-//#ifndef REPTILES_H_
+ * Proyecto TC1033
+ * Daniel Emilio Fuentes Portaluppi
+ * A01708302
+ * 03/12/21
+ */
+
 #define REPTILES_H_
 
 #include<string>
@@ -8,36 +13,11 @@ using namespace std;
 
 class Reptil: public Animal
 {
-    private:
-        int piel;
-
+    
     public:
         //Constructor
         Reptil(){}
-        Reptil(string esp, int pob, int sk): Animal(esp, pob, 0){
-            piel = sk;
+        Reptil(string esp, int pob): Animal(esp, pob, 0){
         }
 
-    //Getters
-    int getPiel(){
-        return piel;
-    }
-
-    void mudaPiel(int tiempo) //Función que calcula aproximadamente cuantas veces un miembro de la población mudó de piel.
-    {
-        piel = 12 * tiempo;
-    }
-
-    void aumentaPoblacionReptiles(int tiempo) //Función que calcula aproximadamente el crecimiento de la población de la especie.
-    {
-        while(i < tiempo)
-        {
-            mortalidad = poblacion - (poblacion / 9);
-            poblacion = mortalidad + (poblacion / 3);
-            i = i + 1;
-        }
-        years = years + tiempo;
-    }
-    
 };
-
