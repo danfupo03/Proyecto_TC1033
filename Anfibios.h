477b3c3ecfdc049dffcf0da732c6159d13e0b5d1
Daniel Fuentes <94033808+danfupo03@users.noreply.github.com>
2021-12-02 17:15:53 -0600

    Update Anfibios.h

--- Anfibios.h
+++ Anfibios.h
@@ -1,29 +1,24 @@
+/*
+ * Proyecto TC1033
+ * Daniel Emilio Fuentes Portaluppi
+ * A01708302
+ * 03/12/21
+ */
+
 #define ANFIBIOS_H_
 
 #include<string>
 #include "Animales.h"
 
-
 using namespace std;
 
 class Anfibio: public Animal
-{    
+{
+
     public:
-        //Constructores
+        //Constructor
         Anfibio(){}
         Anfibio(string esp, int pob): Animal(esp, pob, 0){
         }
-
-
-    void aumentaPoblacionAnfibios(int tiempo) //Función que calcula aproximadamente el crecimiento de la población de la especie.
-    {
-        while(i < tiempo)
-        {
-            mortalidad = poblacion - (poblacion * 0.5);
-            poblacion = mortalidad + (poblacion * 2);
-            i = i + 1;
-        }
-        years = years + tiempo;
-    }
     
 };
